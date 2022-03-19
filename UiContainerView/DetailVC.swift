9e6e159c313aa491f199d902975c0a93dac0f415
Tony Cherevko <anton.ch.tr@gmail.com>
2022-03-19 08:10:25 +0700

    Приложение с музыкой для айпада

---
+++ UiContainerView/DetailVC.swift
@@ -0,0 +1,52 @@
+//
+//  DetailVC.swift
+//  UiContainerView
+//
+//  Created by Tony on 19.03.2022.
+//
+
+import UIKit
+
+class DetailVC: UITableViewController {
+    let imageNameArray = ["Alberto Ruiz - 7 Elements (Original Mix)",
+                          "Dave Wincent - Red Eye (Original Mix)",
+                          "E-Spectro - End Station (Original Mix)",
+                          "Edna Ann - Phasma (Konstantin Yoodza Remix)",
+                          "Ilija Djokovic - Delusion (Original Mix)",
+                          "John Baptiste - Mycelium (Original Mix)",
+                          "Lane 8 - Fingerprint (Original Mix)",
+                          "Mac Vaughn - Pink Is My Favorite Color (Alex Stein Remix)",
+                          "Metodi Hristov, Gallya - Badmash (Original Mix)",
+                          "Veerus, Maxie Devine - Nightmare (Original Mix)"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return imageNameArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "title", for: indexPath)
+        
+        cell.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+        cell.textLabel?.text = imageNameArray[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let viewContriller = parent as? ViewController {
+            let trackTitle = imageNameArray[indexPath.row]
+            viewContriller.titleLabel.text = trackTitle
+            viewContriller.imageCover.image = UIImage(named: trackTitle)
+        }
+    }
+}
--- UiContainerView/ViewController.swift
+++ UiContainerView/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageCover: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
